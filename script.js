d465deef17707bbc45302a936474490f527d9570
Debbie999DAE <debreynolds8888@gmail.com>
2023-04-13 17:19:02 -0400

    all files from module three

---
+++ script.js
@@ -0,0 +1,36 @@
+/* this is a question regarding how many times people have voted*/
+
+var userScore;
+var passGoal = 5;
+userScore = 3;
+  
+if ( userScore < passGoal ) {
+  console.log( "you have not met the threshold" + userScore + "stay engaged");
+}  else{
+  console.log( "Glad you are here! Welcome!" );
+}
+
+
+function howmanytimesvoted(userScore,passGoal){
+  
+}
+
+if ( userScore < passGoal ) {
+  return ( ""you have not met the threshold"" + userScore + "stay engaged"  );
+} else {
+  return(  " Glad you are here! Welcome!); 
+}
+var firstNumber;
+var secondNumber;
+var result;
+
+firstNumber = 10;
+secondNumber = 5;
+result = firstNumber + secondNumber;
+alert( result);
+
+var message = howmanytimesvoted(  2, 10);
+alert( message);
+console.log( howmanytimesvoted(1,10));
+var h3Element=document.getElementById(" resultOutput");
+h3Element.innerHTML ="resultsMessage";
